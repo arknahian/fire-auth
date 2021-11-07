b58fa9c1afb01ad95b0ef862b19ef4ff3c17d81e
Al Nahian Ark <63243119+arknahian@users.noreply.github.com>
2021-11-07 17:40:22 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from "./firebase.config";
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword  } from "firebase/auth";
 import { useState } from 'react';
 initializeApp(firebaseConfig);
 
 function App() {
+  const [newUser, setNewUser] = useState(false);
   const [user, setUser] = useState({
     isSignedIn : false,
+    name : '',
     photo : '',
-    email : ''
+    email : '',
+    password : ''
   });
   const auth = getAuth();
   const provider = new GoogleAuthProvider();
@@ -41,6 +44,73 @@ function App() {
       console.log(error.message)
     });
   }
+
+  const handleBlur = (event) => {
+    let isValid;
+    if (event.target.name === 'email') {
+      const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(event.target.value)
+      isValid = isEmailValid;
+    }
+    if (event.target.name === 'password') {
+      const isPassValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/.test(event.target.value);
+      isValid = isPassValid ;
+    }
+    if (event.target.name === 'name') {
+      isValid = true ;
+    }
+    if (isValid) {
+      const newUserInfo = {...user};
+      newUserInfo[event.target.name] = event.target.value;
+      newUserInfo["isSignedIn"] = true;
+      newUserInfo.name = event.target.value;
+      setUser(newUserInfo);
+      console.log(user)
+    }
+  }
+
+  const handleSubmit = (event) => {
+    if (user.isSignedIn) {
+      createUserWithEmailAndPassword(auth, user.email, user.password)
+  .then((userCredential) => {
+    // Signed in 
+    const signedUserInfo = userCredential.user;
+    console.log(signedUserInfo);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorCode, errorMessage)
+  });
+
+    }
+    event.preventDefault();
+  }
+  
+  const handleLogIn = (event) => {
+    signInWithEmailAndPassword(auth, user.email, user.password)
+  .then((userCredential) => {
+    // Signed in 
+    const loggedInUser = userCredential.user;
+    const {name, password, email} = loggedInUser;
+    setUser({
+      isSignedIn : true,
+     name: name,
+     email : email,
+     password : password
+    })
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorCode, errorMessage);
+  });
+  event.preventDefault();
+  }
+
+  
+
+
+
   const {name, photo, email, isSignedIn} = user;
   return (
     <div className="App">
@@ -54,7 +124,32 @@ function App() {
        </div>
        : <button onClick={handleSignIn}>Sign In</button>
      }
-     
+
+
+     <form action="">
+       <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id="" />
+       <label htmlFor="">New User Sign Up</label>
+       <br />
+       {
+         newUser && <input onBlur={handleBlur} type="text" name="name" placeholder="Your Name"/>
+       }
+       <br />
+       <input onBlur={handleBlur} type="email" placeholder="Enter Your Email" name="email" required/>
+       <br />
+       <input onBlur={handleBlur} type="password" name="password" id="" placeholder="Enter your Password" required/>
+       <br />
+       {
+         newUser ? <input onClick={handleSubmit} type="submit" value="Submit" />
+         : <input type="submit" onClick={handleLogIn} value="Log-In" />
+       }
+       {
+         user ? user.password && user.email ? <p>user {newUser ? "created" : "logged in"} successfully</p> : 
+         <p style={{color: "red"}}>Something went wrong</p>
+         : ''
+       }
+
+     </form>
+     <h1>nodes</h1>
     </div>
   );
 }
